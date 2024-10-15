979d9e06f77cd1dbf6e3ecb799bbdc00e7b94b86
pivnati <natanmuleta911@gmail.com>
2024-10-15 13:13:45 -0700

    install the fire base

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hello world.js')
+const firebaseConfig = {
+
+    apiKey: "AIzaSyA_69H-0GjNDNBvT2QGmKdACXJXxc-Ix7M",
+  
+    authDomain: "fir-9-dojo-95a6f.firebaseapp.com",
+  
+    projectId: "fir-9-dojo-95a6f",
+  
+    storageBucket: "fir-9-dojo-95a6f.appspot.com",
+  
+    messagingSenderId: "700153593412",
+  
+    appId: "1:700153593412:web:ccbadb9944eb92cde98611"
+  
+  };
